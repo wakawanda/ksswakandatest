e359ed5ef4cc4595c9e947c4b9eae3585bfeeb2d
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-04-10 11:57:15 -0400

    Initial commit

---
+++ Test1/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var checkbox2 = {};	// @checkbox
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	checkbox2.click = function checkbox2_click (event)// @startlock
+	{// @endlock
+		sources.disclosures.query('DisclosureStatus =:1','New');
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("checkbox2", "click", checkbox2.click, "WAF");
+// @endregion
+};// @endlock
