8c67b99c845dd56b08540710fe2a073d632fd574
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-04-11 12:46:10 -0400

    Update #7

--- Test1/WebFolder/index.waPage/scripts/index.js
+++ Test1/WebFolder/index.waPage/scripts/index.js
@@ -3,16 +3,35 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
 	var checkbox2 = {};	// @checkbox
+	var checkbox1 = {};	// @checkbox
+	var menuItem2 = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
 	checkbox2.click = function checkbox2_click (event)// @startlock
 	{// @endlock
-		sources.disclosures.query('DisclosureStatus =:1','New');
+		// Add your code here
+	};// @lock
+
+	checkbox1.click = function checkbox1_click (event)// @startlock
+	{// @endlock
+			if($$('checkbox1').getValue()){
+				sources.disclosures.all();
+			}
+			else{
+				sources.disclosures.query('DisclosureStatus =:1','New');
+			}
+	};// @lock
+
+	menuItem2.click = function menuItem2_click (event)// @startlock
+	{// @endlock
+		$$("containerDisclosureFilter").hide();
 	};// @lock
 
 // @region eventManager// @startlock
 	WAF.addListener("checkbox2", "click", checkbox2.click, "WAF");
+	WAF.addListener("checkbox1", "click", checkbox1.click, "WAF");
+	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
 // @endregion
 };// @endlock
